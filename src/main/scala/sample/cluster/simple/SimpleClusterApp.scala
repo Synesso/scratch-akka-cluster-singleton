4ae89b2a0497610107a6005879c537be157a663a
Jem Mawson <jem.mawson@gmail.com>
2014-03-31 16:18:49 +1100

    removed multi port args parsing

--- src/main/scala/sample/cluster/simple/SimpleClusterApp.scala
+++ src/main/scala/sample/cluster/simple/SimpleClusterApp.scala
@@ -6,24 +6,20 @@ import akka.actor.Props
 
 object SimpleClusterApp {
   def main(args: Array[String]): Unit = {
-    if (args.isEmpty)
-      startup(Seq("2551", "2552", "0"))
-    else
-      startup(args)
+    startup(args.headOption.getOrElse("0"))
   }
 
-  def startup(ports: Seq[String]): Unit = {
-    ports foreach { port =>
-      // Override the configuration of the port
-      val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
-        withFallback(ConfigFactory.load())
+  def startup(port: String): Unit = {
+    // Override the configuration of the port
+    val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
+      withFallback(ConfigFactory.load())
 
-      // Create an Akka system
-      val system = ActorSystem("ClusterSystem", config)
-      // Create an actor that handles cluster domain events
-      system.actorOf(Props[SimpleClusterListener], name = "clusterListener")
-      system.actorOf(Props[SimpleClusterSingleton], name = "clusterSingleton")
-    }
+    // Create an Akka system
+    val system = ActorSystem("ClusterSystem", config)
+    // Create an actor that handles cluster domain events
+    system.actorOf(Props[SimpleClusterListener], name = "clusterListener")
+
+    system.actorOf(Props[SimpleClusterSingleton], name = "clusterSingleton")
   }
 
 }
