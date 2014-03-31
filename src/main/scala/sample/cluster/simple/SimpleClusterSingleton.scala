8d3fd4ead323c87d15a1ee2d25769ff921b31289
Jem Mawson <jem.mawson@gmail.com>
2014-04-01 09:22:38 +1100

    pong 10 times

--- src/main/scala/sample/cluster/simple/SimpleClusterSingleton.scala
+++ src/main/scala/sample/cluster/simple/SimpleClusterSingleton.scala
@@ -1,18 +1,25 @@
 package sample.cluster.simple
 
-import akka.actor.{ActorLogging, Actor}
+import akka.actor.{Props, ActorLogging, Actor}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
+import sample.cluster.simple.ClusterTestProtocol.Ping
 
-class SimpleClusterSingleton extends Actor with ActorLogging with ClusterTestProtocol {
+class SimpleClusterSingleton extends Actor with ActorLogging {
   override def receive: Actor.Receive = {
-    case Ping => log.info("Pong!")
+    case Ping => {
+      log.info("Ping?")
+      (1 to 10) foreach {_ =>
+        val ponger = context.actorOf(Props(classOf[PongingActor]))
+        ponger ! Ping
+      }
+    }
   }
 
   context.system.scheduler.schedule(0.seconds, 10.seconds, self, Ping)
 }
 
-trait ClusterTestProtocol {
+object ClusterTestProtocol {
   case object Ping
 }
