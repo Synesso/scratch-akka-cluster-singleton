d588cba59a8d69d49cb0e439c429c4d73f2a54e0
Jem Mawson <jem.mawson@gmail.com>
2014-04-03 11:09:09 +1100

    working cluster across machines

--- src/main/scala/sample/cluster/simple/SimpleClusterSingleton.scala
+++ src/main/scala/sample/cluster/simple/SimpleClusterSingleton.scala
@@ -1,17 +1,18 @@
 package sample.cluster.simple
 
-import akka.actor.{Props, ActorLogging, Actor}
+import akka.actor.{Deploy, Props, ActorLogging, Actor}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
 import sample.cluster.simple.ClusterTestProtocol.Ping
+import akka.cluster.ClusterScope
 
 class SimpleClusterSingleton extends Actor with ActorLogging {
   override def receive: Actor.Receive = {
     case Ping => {
       log.info("Ping?")
       (1 to 10) foreach {_ =>
-        val ponger = context.actorOf(Props(classOf[PongingActor]))
+        val ponger = context.actorOf(Props[PongingActor].withDeploy(Deploy(scope = ClusterScope)))
         ponger ! Ping
       }
     }
